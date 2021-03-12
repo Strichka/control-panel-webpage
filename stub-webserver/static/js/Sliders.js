f17c824e51db5423ef7e3e33b3c917a7767c9e8c
Vitalii Petrunenko <vitaliipetrunenko@gmail.com>
2021-03-12 22:26:13 +0200

    Added mode selection

--- stub-webserver/static/js/Sliders.js
+++ stub-webserver/static/js/Sliders.js
@@ -1,15 +1,18 @@
- 
+
 let sliders = document.querySelectorAll('input[name=rangeInput]');
-let PLUSbtn=document.getElementById("PLUSbtn");
-let MINUSbtn=document.getElementById("MINUSbtn");
+let PLUSbtn = document.getElementById("PLUSbtn");
+let MINUSbtn = document.getElementById("MINUSbtn");
 
 var output = document.querySelectorAll("span.SlideOut");
 let Dropmenu = document.getElementById("Menu1");
 
+var ModeSelectors = document.getElementsByName('ModeSelector')
+var ModeIndex;
+
 var sliderSettings;
 //document.body.addEventListener('input', enforce_maxlength);
 window.addEventListener('load', (event) => {
-    
+
 
 
     var GETsliders = new XMLHttpRequest();
@@ -18,16 +21,18 @@ window.addEventListener('load', (event) => {
 
     GETsliders.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            
-            sliderSettings = JSON.parse(this.responseText)
 
+            sliderSettings = JSON.parse(this.responseText)
+            console.log(sliderSettings);
             sliders[0].value = sliderSettings.brightness;
             sliders[1].value = sliderSettings.speed;
             sliders[2].value = sliderSettings.width;
             sliders[3].value = sliderSettings.led_count;
+            ModeIndex = sliderSettings.mode_index;
+            ModeSelectors[ModeIndex].classList.add("Active");
             sliders.forEach((element, i) => {
                 output[i].innerHTML = element.value;
-                
+
             });
 
         }
@@ -57,51 +62,70 @@ console.log(output);
 
 sliders.forEach((element, i) => {
     element.addEventListener("input", () => {
-        
+
         enforce_maxlength(event);
         arrS[i].textContent = ` .slidecontainer:nth-child(${i + 1}) .slider::-webkit-slider-thumb{background-color: hsl(${100 - (element.value / (element.max / 100))}, 100%, 50%)} `
-        
-        
-        
+
+
+
         output[i].innerHTML = element.value;
-        
-        ChangeSlideData(element,i);
+
+        ChangeSlideData(element, i);
     })
 });
 
-let ChangeSlideData=((element,i) => {
+let ChangeSlideData = ((element, i) => {
 
     var changeData = new XMLHttpRequest();
-        changeData.open("PATCH", '/led/config', true);
-        
-        let val = sliderSettings;
-        if(i<3){
-        val[Object.keys(sliderSettings)[i + 1]] = parseInt(element.value, 10);  
-        }
-        else{
-            val[Object.keys(sliderSettings)[i + 2]] = parseInt(element.value, 10); 
-        }
-        console.log(val);
-        changeData.send(JSON.stringify(val));
+    changeData.open("PATCH", '/led/config', true);
+
+    let val = sliderSettings;
+    if (i < 3) {
+        val[Object.keys(sliderSettings)[i + 1]] = parseInt(element.value, 10);
+    }
+    else {
+        val[Object.keys(sliderSettings)[i + 2]] = parseInt(element.value, 10);
+    }
+    console.log(val);
+    changeData.send(JSON.stringify(val));
 
 });
 //
-PLUSbtn.addEventListener("mousedown",(event) => {
-    sliders[3].value = parseInt(sliders[3].value)+500;
+PLUSbtn.addEventListener("mousedown", (event) => {
+    sliders[3].value = parseInt(sliders[3].value) + 500;
     var event = new Event('input', {});
-    
+
     sliders[3].dispatchEvent(event);
 });
-MINUSbtn.addEventListener("mousedown",(event) => {
-        sliders[3].value = parseInt(sliders[3].value)-500;
+MINUSbtn.addEventListener("mousedown", (event) => {
+    sliders[3].value = parseInt(sliders[3].value) - 500;
     var event = new Event('input', {});
     sliders[3].dispatchEvent(event);
- 
-    }
-);
- 
 
-let checker=MINUSbtn.addEventListener("mousedown",(event) => {return true;});
+}
+);
+
+ModeSelectors.forEach((element ,i) => {
+    element.addEventListener("click", () => {
+        ModeIndex = i;
+        console.log(ModeSelectors);
+        if(!(ModeSelectors[i].classList.contains("Active")))
+        {
+        ModeSelectors[i].classList.add("Active");
+        ModeSelectors[i].value = i;
+        ChangeSlideData(ModeSelectors[i],-1)
+            ModeSelectors.forEach((element,i) => {
+                if(ModeIndex !== i){
+                    element.classList.remove("Active");
+                }
+                
+            });
+
+
+        }
+
+    });
+})
 
 
 
