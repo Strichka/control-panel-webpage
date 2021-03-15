1c397a48271a573e14b20854ce8c7a049a72d213
Vitalii Petrunenko <vitaliipetrunenko@gmail.com>
2021-03-15 18:10:07 +0200

    fixed <hr> bug

--- stub-webserver/static/js/Popup.js
+++ stub-webserver/static/js/Popup.js
@@ -20,7 +20,7 @@ window.addEventListener('load', (event) => {
 
                 body = document.getElementById("Body")
                 var popup = document.createElement("div");
-                popup.innerHTML = "<div class='textbox'>Привет! <br> Мы бы хотели собрать всю твою личную информацию<div><button class='button' id='Agree'>согласен</button><button class='button' id='Disagree'>не согласен</button></div></div>";
+                popup.innerHTML = "<div class='textbox'>Привет! <br> Мы бы хотели собрать всю твою личную информацию<div class='popupButtonPanel'><button class='button' id='Agree'>согласен</button><button class='button' id='Disagree'>не согласен</button></div></div>";
                 popup.className = "Popup";
                 document.body.appendChild(popup);
 
